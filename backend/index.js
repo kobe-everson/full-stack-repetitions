91c0fa06fa88deacc0401eaa7f9343e54d1babcd
kobe-everson <everson.kobe@gmail.com>
2026-04-02 12:17:44 -0500

    all CRUD operations implemented

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,57 @@ app.get("/movies", (req, res) => {
     );
 });
 
+app.post("/movies", (req, res) => {
+  const newMovie = req.body;
+  knex("favorites")
+    .insert(
+      {
+        title: newMovie.title,
+        main_character: newMovie.main_character,
+        year_released: newMovie.year_released,
+      },
+      ["id", "title", "main_character", "year_released"],
+    )
+    .then((insertedMovie) => {
+      res.status(201).json(insertedMovie[0]);
+    })
+    .catch((err) => {
+      console.error("Error adding movie", err);
+      res.status(500).json({ message: "Unable to add movie" });
+    });
+});
+
+app.delete("/movies/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedCount = await knex("favorites").del().where({ id });
+    if (deletedCount === 0) {
+      return res.status(404).json({ message: "Movie not found" });
+    }
+    res.status(200).json({ message: "Movie delete successful" });
+  } catch (err) {
+    console.error("Unable to delete movie", err);
+    res.status(500).json({ message: "Failed to delete movie" });
+  }
+});
+
+app.patch("/movies/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+    const updated = await knex("favorites")
+      .update(updates, ["id", "title", "main_character", "year_released"])
+      .where({ id });
+    if (updated.length === 0) {
+      return res.status(404).json({ message: "Movie not found" });
+    }
+    res.status(200).json(updated[0]);
+  } catch (err) {
+    console.error("Error deleting movie", err);
+    res.status(500).json({ message: "Failed to update movie" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
